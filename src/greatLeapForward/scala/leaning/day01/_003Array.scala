c7816f6721088a3028c769267bf1d5d1161fa256
Jinbin-Zhu <asjinbinzhu@gmail.com>
2017-03-13 18:12:06 -0700

    day01

---
+++ src/greatLeapForward/scala/leaning/day01/_003Array.scala
@@ -0,0 +1,54 @@
+package greatLeapForward.scala.leaning.day01
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * Created by johann on 3/13/17.
+  */
+class _003Array {
+
+}
+object _003Array extends App{
+
+  //10, 是数组长度
+  val arr1 = new Array[Int](10)
+
+  //调用了Array的apply方法，apply会new数组实例并将值赋给数组实例
+  val arr2 = Array(1, 2, 3)
+
+  //8, 是数组元素
+  val arr3 = Array[Int](8)
+
+  //可变长度数组
+  val abuf = new ArrayBuffer[Int]()
+
+  abuf += 2 //添加单个元素
+  abuf += 4
+  abuf += 6
+  abuf += (7, 8, 9) //添加元组
+  abuf ++= arr2     //添加数组
+  abuf.insert(2, 99, 99, 100) //在(2)处添加99，99，100
+  abuf.remove(1, 3) //从（1）处开始，删除3个元素
+
+
+  //print(abuf.toList)
+  abuf.map(println)
+
+  //map返回值类型，由前面表达式确定
+  val abuf2 = abuf.map(_ * 2)
+  abuf2.map(println)
+
+  /**
+    * 数组常用方法
+    */
+
+  println(abuf.sum)
+  println(abuf.min)
+  println(abuf.max)
+  println(abuf.length)
+  println(abuf.last)
+  abuf.sorted.map(println)
+  //abuf.sortBy(_)
+  abuf.sortWith(_ > _).map(println)
+
+}
---
+++ src/greatLeapForward/scala/leaning/day01/_004Collection.scala
@@ -0,0 +1,41 @@
+package greatLeapForward.scala.leaning.day01
+
+/**
+  * Created by johann on 3/13/17.
+  */
+class _004Collection {
+
+}
+
+object _004Collection extends App{
+
+  /**
+    * Map
+    */
+  import scala.collection.mutable._
+  val map = Map("name" -> "zhousan", "age" -> 18)
+  //map("name") = "lisi"  //scala集合类分为mutable（值可变）和immutable（值不可变). 默认为immutable
+  map("age") = 21
+
+  //添加元素
+  map("score") = 59.5
+  map += ("gender" -> "male")
+
+
+  /**
+    * Tuple
+    */
+  val stu = ("zhousan", 18, 59.5)
+  println(stu._1) //角标从1开始
+
+  val arr = Array(("name", "zhangsan"), ("age", 21))
+  arr.toMap
+
+  /**
+    * Zip
+    */
+  val a = Array("name", "age", "class") //如果数组长度不一致，则多出来的部分去掉
+  val b = Array("zhousan", 17, 3)
+  a.zip(b)
+
+}
